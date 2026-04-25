1d2f59b0d5333ed6026916436ff02a5b89d91fa3
Sneha Mohalanobish <sneha.iembca2026.2006@gmail.com>
2026-04-25 12:52:53 +0530

    Update Firebase configuration with placeholders

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABiceNcqi4onUflikpg0vkA1sNpW-wzmU",
+  apiKey: "YOUR-API-KEY",
   authDomain: "gym-management-system-f1bea.firebaseapp.com",
   projectId: "gym-management-system-f1bea",
   storageBucket: "gym-management-system-f1bea.appspot.com",
-  messagingSenderId: "507559405778",
-  appId: "1:507559405778:web:a382544b6c2d9361bbe50b"
+  messagingSenderId: "YOUR-ID",
+  appId: "YOUR-ID"
 };
 
 const app = initializeApp(firebaseConfig);
